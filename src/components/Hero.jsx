346047df7ff98781ad4d3aa8ac25cf17190f935f
sarthakroutray <sarthak.routray2006@gmail.com>
2025-11-03 09:54:35 +0530

    Revamp 3/11/25

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto flex items-center justify-center`}>
       <div
-        className={`max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-center justify-center gap-5`}
+        className={`max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start justify-center gap-5`}
       >
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div className='text-center'>
+        <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Sarthak</span>
           </h1>
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,11 +1,11 @@
-import { useState, useRef, Suspense } from "react";
+import { useState, useRef, Suspense, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = (props) => {
+const Stars = ({ numStars = 5000, ...props }) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() => random.inSphere(new Float32Array(numStars), { radius: 1.2 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -28,11 +28,28 @@ const Stars = (props) => {
 };
 
 const StarsCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className='w-full h-full' style={{ pointerEvents: 'none' }}>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
-          <Stars />
+          <Stars numStars={isMobile ? 1000 : 5000} />
         </Suspense>
 
         <Preload all />
